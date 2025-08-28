8bf93972beb1af638372f98636c30c49f65bc2c5
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2025-08-28 18:07:18 +0200

    Added SDUI scope for project

--- packages/app/composeApp/src/commonMain/kotlin/nl/q42/ui/composition/ScreenComposer.kt
+++ packages/app/composeApp/src/commonMain/kotlin/nl/q42/ui/composition/ScreenComposer.kt
@@ -3,6 +3,7 @@ package nl.q42.ui.composition
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import nl.q42.common.*
 import nl.q42.common.screen.Screen
 import nl.q42.ui.components.*
